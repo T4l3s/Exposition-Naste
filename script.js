127a50b115ebe91545f54c872407a16dc59a9570
Paulo Vitor <paulomenezes8499@gmail.com>
2023-09-19 00:54:45 -0400

    Primeiro e último commit

---
+++ script.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollHorizontally: true,
+    anchors: ['inicio', 'produtos', 'artes', 'sobre'],
+    responsiveHeight: 410,
+    continuousVertical: true,
+})
